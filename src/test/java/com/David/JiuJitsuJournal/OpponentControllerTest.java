1d2a897098544bb541b6b68d7a7748057b50639f
davidryee <davidryee@gmail.com>
2021-02-15 16:59:49 -0800

    GET Opponent by Id junit test is working

--- src/test/java/com/David/JiuJitsuJournal/OpponentControllerTest.java
+++ src/test/java/com/David/JiuJitsuJournal/OpponentControllerTest.java
@@ -1,15 +1,19 @@
 package com.David.JiuJitsuJournal;
 
 import com.David.JiuJitsuJournal.api.controllers.OpponentController;
+import com.David.JiuJitsuJournal.data.entities.BeltRank;
+import com.David.JiuJitsuJournal.data.entities.User;
+import com.David.JiuJitsuJournal.data.repository.OpponentRepository;
+import com.David.JiuJitsuJournal.data.repository.UserRepository;
 import com.David.JiuJitsuJournal.domain.BeltRankEnum;
 import com.David.JiuJitsuJournal.domain.managers.OpponentManager;
-import com.David.JiuJitsuJournal.domain.models.Opponent;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -17,7 +21,10 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -25,9 +32,12 @@ import static org.mockito.Mockito.when;
 public class OpponentControllerTest {
     @InjectMocks
     private OpponentController opponentController;
+    private OpponentManager opponentManager;
 
     @Mock
-    private OpponentManager opponentManager;
+    private OpponentRepository opponentRepository;
+    @Mock
+    private UserRepository userRepository;
 
     @Before
     public void init() {
@@ -35,6 +45,23 @@ public class OpponentControllerTest {
     }
     @Test
     public void getOpponentByIdShouldReturnOpponentIfItExists() {
+        User userEntity = new User();
+        when(userRepository.findByUsername(null)).thenReturn(java.util.Optional.of(userEntity));
+
+        long opponentId = 1L;
+
+        com.David.JiuJitsuJournal.data.entities.Opponent opponentEntity = new com.David.JiuJitsuJournal.data.entities.Opponent();
+        opponentEntity.setUser(userEntity);
+        opponentEntity.setBeltRank(new BeltRank(1, "Brown"));
+        opponentEntity.setHeightInInches(72);
+        opponentEntity.setWeightInLbs(176);
+        opponentEntity.setName("Royce Gracie");
+        opponentEntity.setId(1L);
+        when(opponentRepository.findOne(any(Specification.class))).thenReturn(Optional.of(opponentEntity));
+
+
+        opponentManager = new OpponentManager(new com.David.JiuJitsuJournal.data.services.OpponentDataService(opponentRepository, userRepository));
+        opponentController = new OpponentController(opponentManager);
         UserDetails userDetails = mock(UserDetails.class);
         Authentication authentication = mock(Authentication.class);
         SecurityContext securityContext = mock(SecurityContext.class);
@@ -42,14 +69,15 @@ public class OpponentControllerTest {
         SecurityContextHolder.setContext(securityContext);
         when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(userDetails);
 
-        Opponent domainOpponent = new Opponent("Royce Gracie",
-                                                     BeltRankEnum.BROWN,
-                                                    73,
-                                                     176,
-                                                  1L);
-        when(opponentManager.getOpponentById(1L, userDetails.getUsername())).thenReturn(domainOpponent);
-        ResponseEntity responseEntity = opponentController.getOpponent(1L);
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 
+        ResponseEntity responseEntity = opponentController.getOpponent(opponentId);
+        com.David.JiuJitsuJournal.api.responses.Opponent opponentDto = (com.David.JiuJitsuJournal.api.responses.Opponent) responseEntity.getBody();
+
+        assertEquals(Optional.of(opponentEntity.getId()).get(), opponentDto.getId());
+        assertEquals(opponentEntity.getName(), opponentDto.getName());
+        assertEquals(opponentEntity.getHeightInInches(), opponentDto.getHeightInInches());
+        assertEquals(opponentEntity.getWeightInLbs(), opponentDto.getWeightInLbs());
+        assertEquals(BeltRankEnum.values()[opponentEntity.getBeltRank().getBeltRankId()], opponentDto.getBeltRank());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
     }
 }
